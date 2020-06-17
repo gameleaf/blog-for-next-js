e63087b4118d777f8142764a25dbc69de86e981d
Ron Yeh <ronyeh@gmail.com>
2020-06-16 18:09:09 -0700

    Implement getStaticPaths().

--- index.ts
+++ index.ts
@@ -5,46 +5,47 @@ type BlogPostsConfig = {
     postsRoot?: string;
     patternToAccept?: RegExp;
     patternToReject?: RegExp;
+    pageParamName?: string;
 };
 
 const DEFAULT_POSTS_DIR: string = "posts";
+const DEFAULT_PAGE_PARAM_NAME: string = "slug";
 const DEFAULT_PATTERN_TO_ACCEPT: RegExp = /\.md$/;
 
-const DEFAULT_BLOG_POSTS_CONFIG: BlogPostsConfig = {
-    postsRoot: DEFAULT_POSTS_DIR,
-    patternToAccept: DEFAULT_PATTERN_TO_ACCEPT,
-    patternToReject: undefined,
+type BlogPostsConfigInternal = {
+    postsRoot: string;
+    patternToAccept: RegExp;
+    patternToReject: RegExp | undefined;
+    pageParamName: string;
 };
 
 class BlogPosts {
+    private config: BlogPostsConfigInternal = {
+        postsRoot: DEFAULT_POSTS_DIR,
+        patternToAccept: DEFAULT_PATTERN_TO_ACCEPT,
+        patternToReject: undefined,
+        pageParamName: DEFAULT_PAGE_PARAM_NAME,
+    };
+
     postsRootAbsolutePath: string = "";
     slugToAbsolutePath: Record<string, string> = {};
     blogCacheDirectory: string = "";
+    hasPatternToReject: Boolean = false;
 
-    constructor(config?: BlogPostsConfig) {
-        // If the user does not pass in any config object, we use the defaults.
-        if (!config) {
-            config = DEFAULT_BLOG_POSTS_CONFIG;
-        }
-        // If any properties are missing, we fill them in with the defaults.
-        if (typeof config.postsRoot === "undefined") {
-            config.postsRoot = DEFAULT_POSTS_DIR;
-        }
-        if (typeof config.patternToAccept === "undefined") {
-            config.patternToAccept = DEFAULT_PATTERN_TO_ACCEPT;
-        }
-        const patternToAccept = config.patternToAccept;
-        let patternToReject: RegExp | undefined;
-        if (typeof config.patternToReject !== "undefined") {
-            patternToReject = config.patternToReject;
+    constructor(userConfig?: BlogPostsConfig) {
+        if (userConfig) {
+            this.loadConfig(userConfig);
         }
 
-        this.postsRootAbsolutePath = path.join(process.cwd(), config.postsRoot);
+        this.postsRootAbsolutePath = path.join(process.cwd(), this.config.postsRoot);
 
         this.slugToAbsolutePath = {};
 
         // Start from the top folder of this.postsRootAbsolutePath
         let itemsToExamine: string[] = [""];
+
+        const hasPatternToReject = this.hasPatternToReject;
+        const patternToAccept = this.config.patternToAccept;
 
         // All of the folders we examine will be relative to this.postsRootAbsolutePath
         while (itemsToExamine.length > 0) {
@@ -65,19 +66,50 @@ class BlogPosts {
                 }
             } else {
                 // FILE
-                if (patternToReject && patternToReject.test(currItemRelativePath)) {
+                if (hasPatternToReject && this.config.patternToReject!.test(currItemRelativePath)) {
                     // REJECTED due to patternToReject.
+                    console.log("Rejected: " + currItemRelativePath);
                 } else if (patternToAccept.test(currItemRelativePath)) {
                     // MATCHES patternToAccept.
                     const slug = currItemRelativePath.replace(patternToAccept, "");
                     this.slugToAbsolutePath[slug] = currItemAbsolutePath;
+                    console.log("Matched: " + currItemRelativePath);
                 } else {
                     // DOES NOT MATCH patternToAccept.
+                    console.log("Ignored: " + currItemRelativePath);
                 }
             }
         }
 
         console.log(this.slugToAbsolutePath);
+    }
+
+    // Override our defaults with the user's supplied options.
+    loadConfig(userConfig: BlogPostsConfig) {
+        if (typeof userConfig.postsRoot !== "undefined") {
+            this.config.postsRoot = userConfig.postsRoot;
+        }
+        if (typeof userConfig.patternToAccept !== "undefined") {
+            this.config.patternToAccept = userConfig.patternToAccept;
+        }
+        if (typeof userConfig.patternToReject !== "undefined") {
+            this.config.patternToReject = userConfig.patternToReject;
+            this.hasPatternToReject = true;
+        }
+        if (typeof userConfig.pageParamName !== "undefined") {
+            this.config.pageParamName = userConfig.pageParamName;
+        }
+    }
+
+    getStaticPaths() {
+        const paths = [];
+        for (const slug in this.slugToAbsolutePath) {
+            const pathToAdd: { params: any } = { params: {} };
+            pathToAdd.params[this.config.pageParamName] = slug.split("/");
+            paths.push(pathToAdd);
+            console.log(pathToAdd);
+        }
+        return paths;
     }
 }
 
