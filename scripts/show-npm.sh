7bde3b2b1185bc241246e8845d86f8772ec5d224
Ron Yeh <ronyeh@gmail.com>
2020-06-23 20:12:39 -0700

    Add scripts to open NPM and GitHub pages.

---
+++ scripts/show-npm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Works on macOS
+open https://www.npmjs.com/package/blog-for-next-js
---
+++ scripts/show-repo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Works on macOS
+open https://github.com/gameleaf/blog-for-next-js
